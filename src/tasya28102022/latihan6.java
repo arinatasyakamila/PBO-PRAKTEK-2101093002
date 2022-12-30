b9c0f9d8c522dc1199f24526b9f3f114ff0fb397
Arina Tasya Kamila <arinatasyakamila@gmail.com>
2022-12-30 17:17:30 +0700

    Create latihan6.java

---
+++ src/tasya28102022/latihan6.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasya28102022;
+
+/**
+ *
+ * @author User
+ */
+public class latihan6 {
+    public static void main(String[] args) {
+        int i;
+        
+        for(i=1; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+}
