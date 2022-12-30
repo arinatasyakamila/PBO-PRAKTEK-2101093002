633ac0d4c7df1e7c03023c962dc07164998bdc5e
Arina Tasya Kamila <arinatasyakamila@gmail.com>
2022-12-30 17:17:39 +0700

    Create Masukkan.java

---
+++ src/tasya28102022/Masukkan.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasya28102022;
+
+/**
+ *
+ * @author User
+ */
+class Masukkan {
+    
+}
