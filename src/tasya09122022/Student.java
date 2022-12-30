a8459fd554b6e5e5f59551992e34b5b06a1023fc
Arina Tasya Kamila <arinatasyakamila@gmail.com>
2022-12-30 17:15:10 +0700

    Create Student.java

---
+++ src/tasya09122022/Student.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasya09122022;
+
+/**
+ *
+ * @author User
+ */
+public class Student extends Person {
+    public Student() {
+        super();
+        System.out.println("Inside Student:Constructor");
+    }
+    
+    public String getName(){ 
+        System.out.println("Parent: getName");
+        return name; 
+    } 
+    //Override
+    
+    public static void main( String[] args ){ 
+        Student student = new Student(); 
+        System.out.println(student.getName());
+        System.out.println(student.getAddress());
+    } 
+}
