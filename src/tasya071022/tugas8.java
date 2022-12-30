9bff618faa5a1202627126b051c4702b09008781
Arina Tasya Kamila <arinatasyakamila@gmail.com>
2022-12-30 17:14:40 +0700

    Create tugas8.java

---
+++ src/tasya071022/tugas8.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasya071022;
+
+/**
+ *
+ * @author User
+ */
+public class tugas8 {//cetak seratus kali menggunakan for-loop
+    public static void main( String [] args){
+        int r;
+        for(r=0; r<100; r++){
+            System.out.println("tasya");
+          
+        }
+    }
+    
+}
