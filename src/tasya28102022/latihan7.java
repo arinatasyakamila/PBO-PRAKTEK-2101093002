a26bb9e95fc091735faa9df0990f9fbe2064bf53
Arina Tasya Kamila <arinatasyakamila@gmail.com>
2022-12-30 17:17:34 +0700

    Create latihan7.java

---
+++ src/tasya28102022/latihan7.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasya28102022;
+
+/**
+ *
+ * @author User
+ */
+public class latihan7 {
+    public static void main( String[] args ){
+        double a = Double.parseDouble (args[2]);
+        double b = Double.parseDouble (args[3]);
+        
+        double Sum, difference, product, quotient;
+        
+        Sum =a+b;
+        difference = a-b;
+        product = a*b;
+        quotient = a/b;
+        
+        System.out.println("Sum ="+Sum);
+        System.out.println("Name : "+difference);
+        System.out.println("Product : "+product);
+        System.out.println("Quotient : "+quotient);
+        
+    }
+    
+}
