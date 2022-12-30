f99a0fde0a9533452dc84c10748277334d34fddc
Arina Tasya Kamila <arinatasyakamila@gmail.com>
2022-12-30 17:14:21 +0700

    Create tugas6.java

---
+++ src/tasya071022/tugas6.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasya071022;
+
+/**
+ *
+ * @author User
+ */
+public class tugas6 {//cetak seratus kali menggunakan while-loop
+    public static void main( String [] args){
+        int r=0;
+        while(r < 100){
+            System.out.println("tasya");
+            r++;
+        }
+    }
+    
+}
