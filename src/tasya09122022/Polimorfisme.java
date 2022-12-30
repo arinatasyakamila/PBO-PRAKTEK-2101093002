f28918b4a1edd25b8abc69ce7b449e3d56251aba
Arina Tasya Kamila <arinatasyakamila@gmail.com>
2022-12-30 17:15:06 +0700

    Create Polimorfisme.java

---
+++ src/tasya09122022/Polimorfisme.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasya09122022;
+
+/**
+ *
+ * @author User
+ */
+public class Polimorfisme {
+   public static main( String[] args ) 
+    { 
+        Person ref; 
+        Student studentObject = new Student(); 
+        Employee employeeObject = new Employee(); 
+        ref = studentObject; //Person menunjuk kepada 
+        // object Student 
+        //beberapa kode di sini 
+     }
+    
+}
