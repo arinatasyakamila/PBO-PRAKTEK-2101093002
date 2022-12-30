a7e1ba11f5c6efe3ff300980b3a91035236d872e
Arina Tasya Kamila <arinatasyakamila@gmail.com>
2022-12-30 17:13:43 +0700

    Create latteori2.java

---
+++ src/tasya071022/latteori2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasya071022;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author User
+ */
+public class latteori2 {
+    public static void main( String[] args ){ 
+ 
+       
+            
+        }
+    
+}
